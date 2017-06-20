5dd3e092d4397891c0c91c0e17a71d29faf8d9ac
Jakub Niedzwiecki <jniedzwiecki@MacBook-Air-Jakub.local>
2017-06-21 00:51:23 +0200

    added daterange module tests

--- tests/testthat/test-daterange.R
+++ tests/testthat/test-daterange.R
@@ -1,1 +1,12 @@
 context('Daterange module')
+
+test_that('date_origin is date vector', {
+  expect_equal(class(date_origin), 'Date')
+  expect_equal(length(date_origin[is.na(date_origin)]), 0)
+})
+
+test_that('daterange consists of jscode, htmlcode and set_date', {
+  expect_true(exists('jscode'))
+  expect_true(exists('htmlcode'))
+  expect_true(exists('set_date'))
+})
