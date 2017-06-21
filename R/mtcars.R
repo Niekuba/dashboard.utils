5c87fa17e8d8cd0fb52112cc8386f7ab9c796b23
Jakub Niedzwiecki <jniedzwiecki@MacBook-Air-Jakub.local>
2017-06-21 08:47:35 +0200

    created "mtcars" dataset documentation

---
+++ R/mtcars.R
@@ -0,0 +1,23 @@
+#' Car parameters
+#'
+#' Compare parameters of multiple cars (names described in rownames)
+#'
+#' @source system.file('data/Rdata.rds', package = 'datasets')
+#' @format Data frame with 32 rows and 11 variables
+#' \describe{
+#' \item{mpg}{Miles per galon}
+#' \item{cyl}{Number of cylinders}
+#' \item{disp}{Displacement (cu. in.)}
+#' \item{hp}{Gross horsepower}
+#' \item{drat}{Rear axle ratio}
+#' \item{wt}{Weight (1000 lbs)}
+#' \item{qsex}{1/4 mile time}
+#' \item{vs}{V/S}
+#' \item{am}{Transmission (0 = automatic, 1 = manual)}
+#' \item{am}{Transmission (0 = automatic, 1 = manual)}
+#' \item{gear}{Number of forward gears}
+#' \item{carb}{Number of carburetors}
+#' }
+#' @examples
+#'   mtcars
+"mtcars"
